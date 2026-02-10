8ffe0bd5fc84af56dd584c1cd0e31c91ca2a9387
ar.sis <70283843+ardotsis@users.noreply.github.com>
2026-02-11 02:34:33 +0900

    update: 2026-02-11 02:34:32 (+09:00)

--- linux/hosts/_default/.config/nvim/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.opt.clipboard = "unnamedplus"
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.keymap.set("x", "p", "P")
-vim.keymap.set("x", "P", "p")
-vim.keymap.set("i", "jj", "<ESC>")
